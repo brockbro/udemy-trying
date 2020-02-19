3fc5f5f6dea2fbcf2a3baad35a65e49a7ab8600b
brockbro <brockbro@hotmail.com>
2020-02-19 21:08:14 +0200

    first commit

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Integer Max value = " + myMaxIntValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte minimum Value = " + myMinByteValue);
+        System.out.println("Byte maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short minimum Value = " + myMinShortValue);
+        System.out.println("Short maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long minimum Value = " + myMinLongValue);
+        System.out.println("Long maximum Value = " + myMaxLongValue);
+
+
+
+        
+
+    }
+}
